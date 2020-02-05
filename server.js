d9d6f6b5c371fde6a5a0fa202697e0e0aa5163f1
Michael Partin <unmykx@gmail.com>
2020-02-04 18:03:25 -0800

    added html routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongojs = require('mongojs');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-
+const path = require('path');
 
 const databaseUrl = "warmup";
 const collections = ["workouts"];
@@ -12,7 +12,7 @@ const db = mongojs[databaseUrl, collections];
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbworkout", { useNewUrlParser: true });
 
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +21,28 @@ app.use(express.static('app/public'));
 
 
 
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
+app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+  
+app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+app.get("/api/exercises", function(req,res) {
+    db.workout.find({})
+        .populate("exercises")
+        .then(dbworkout => {
+        res.json(dbworkout);
+        })
+        .catch(err => {
+        res.json(err);
+        });
+});
 
 
 
