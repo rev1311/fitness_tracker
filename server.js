00611402a7e5027b604a35bb1175cba96b8b5312
Michael Partin <unmykx@gmail.com>
2020-02-04 20:51:24 -0800

    fixed api routes

--- server.js
+++ server.js
@@ -3,28 +3,26 @@ const mongojs = require('mongojs');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
-
-const databaseUrl = "warmup";
-const collections = ["workouts"];
-const db = mongojs[databaseUrl, collections];
-
-
+const db = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbworkout", { useNewUrlParser: true });
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri || "mongodb://localhost/workout", { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('mongodb connected');
+});
 
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('app/public'));
+app.use(express.static(__dirname + '/public'));
 
-
-
-app.get('/', function(req,res) {
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
+  
 app.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
@@ -34,56 +32,56 @@ app.get("/stats", function(req, res) {
 });
 
 
-
-app.get("/api/exercises", function(req,res) {
+app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-        .populate("exercises")
-        .then(dbWorkout => {
+      .populate("exercises")
+      .then(dbWorkout => {
         res.json(dbWorkout);
-        })
-        .catch(err => {
+    })
+      .catch(err => {
         res.json(err);
     });
 });
-
+  
 app.post("/api/workouts", (req, res) => {
     db.Workout.create({ day: Date.now() })
-        .then(dbWorkout => {
+      .then(dbWorkout => {
         console.log(dbWorkout);
         res.json(dbWorkout);
-        })
-        .catch(({ message }) => {
+    })
+      .catch(({ message }) => {
         console.log(message);
         res.json(message);
     });
 });
   
 app.put("/api/workouts/:workout", (req, res) => {
-    let id = req.params.Workout;
+    let id = req.params.workout;
     let data = req.body;
   
     db.Exercise.create(data).then(dbExercise => {
-        console.log(dbExercise);
-        db.Workout.updateOne(
+      console.log(dbExercise);
+      db.Workout.updateOne(
         { _id: mongoose.Types.ObjectId(id) },
-        { $push: { exercises: dbExercise } })
+        { $push: { exercises: dbExercise } }
+    )
         .then(dbWorkout => {
-        res.json(dbWorkout);
+          res.json(dbWorkout);
         })
         .catch(err => {
-        res.json(err);
+          res.json(err);
         });
     });
 });
   
-  app.get("/api/workouts/range", (req, res) => {
+app.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
-        .populate("exercises")
-        .then(dbWorkout => {
+      .populate("exercises")
+      .then(dbWorkout => {
         console.log(dbWorkout);
         res.json(dbWorkout);
-        })
-        .catch(err => {
+    })
+      .catch(err => {
         res.json(err);
     });
 });
